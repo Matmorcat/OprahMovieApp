793a510a68cd45c17b7c63ed3c91a0743d772388
JWeb56 <JWeb56@users.noreply.github.com>
2018-04-09 16:43:38 -0400

    Basic detail menu layout and button behavior
    Created menu for DetailActivity and defined appropriate menu resources

--- app/src/main/java/com/example/android/OprahMovieApp/DetailActivity.java
+++ app/src/main/java/com/example/android/OprahMovieApp/DetailActivity.java
@@ -3,6 +3,7 @@ package com.example.android.OprahMovieApp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,5 +16,22 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle _savedInstanceState) {
         super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    /**
+     * Android callback method to handle action bar item clicks. The action bar will handle
+     * clicks on the Home/Up button
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_add_favorite) {
+
+
+        }
+        
+        return super.onOptionsItemSelected(item);
     }
 }
