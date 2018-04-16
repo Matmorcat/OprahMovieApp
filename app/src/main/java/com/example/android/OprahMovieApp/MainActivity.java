9983d907d5f6e0162e699fc64abdd8c77da8b91a
JWeb56 <JWeb56@users.noreply.github.com>
2018-04-16 18:45:35 -0400

    Removed unused imports

--- app/src/main/java/com/example/android/OprahMovieApp/MainActivity.java
+++ app/src/main/java/com/example/android/OprahMovieApp/MainActivity.java
@@ -15,15 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-
-import org.json.JSONException;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
