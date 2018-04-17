aa7d2a31f9cd36b450b98c526e83eca93c505e56
JWeb56 <JWeb56@users.noreply.github.com>
2018-04-17 16:16:48 -0400

    Basic database functionality for adding favorite movies
    DB branch will have to be merged before we can test this

--- app/src/main/java/com/example/android/OprahMovieApp/DetailActivity.java
+++ app/src/main/java/com/example/android/OprahMovieApp/DetailActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.OprahMovieApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class DetailActivity extends AppCompatActivity {
@@ -29,6 +31,14 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_add_favorite) {
+            //intent to retrieve movie information
+            Intent intent = this.getIntent();
+            if (intent != null && intent.hasExtra(getString(R.string.movie_string))) {
+                Movie movie = (Movie) intent.getSerializableExtra(getString(R.string.movie_string));
+            }
+            //This won't be functional until we merge db branch
+            FDBInterface dataface = new FDBInterface(getApplicationContext());
+            dataface.getConfiguations().add(movie);
             //Toast to display confirmation that movie has been added to favorites
             Toast addFavoritesToast = new Toast(getApplicationContext());
             addFavoritesToast.makeText(getApplicationContext(), R.string.favorites_toast_text, Toast.LENGTH_SHORT).show();
