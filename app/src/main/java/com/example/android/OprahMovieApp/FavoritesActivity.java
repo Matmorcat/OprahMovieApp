84ea7f21f133fe09a4140ebc35ebc8d2001ba2aa
JWeb56 <JWeb56@users.noreply.github.com>
2018-04-18 17:05:20 -0400

    Created FavoritesAdapter class
    -Created FavoritesAdapter class
    -Working on favorites functionality

--- app/src/main/java/com/example/android/OprahMovieApp/FavoritesActivity.java
+++ app/src/main/java/com/example/android/OprahMovieApp/FavoritesActivity.java
@@ -1,15 +1,31 @@
 package com.example.android.OprahMovieApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.OprahMovieApp.data.Movie;
+import com.example.android.OprahMovieApp.favorites.FavoritesModel;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 // TODO: Implement favorites activity - code started
 public class FavoritesActivity extends AppCompatActivity {
+
+    protected List<Movie> favoriteMovies;
 
     @Override
     protected void onCreate(Bundle _savedInstanceState) {
         super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        favoriteMovies = MainActivity.getFavoritesModel().getFavoriteMovies();
+
+
+
 
     }
 }
---
+++ app/src/main/java/com/example/android/OprahMovieApp/data/FavoritesAdapter.java
@@ -0,0 +1,13 @@
+package com.example.android.OprahMovieApp.data;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class FavoritesAdapter extends MovieAdapter {
+    public FavoritesAdapter(Context _context, int _resource, List<Movie> _movies) {
+        super(_context, _resource, _movies);
+    }
+
+    
+}
